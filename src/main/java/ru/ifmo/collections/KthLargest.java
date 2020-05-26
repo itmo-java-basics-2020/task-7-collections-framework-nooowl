bd3e8e474341b57f918f81f763c6ab899df928d2
polina <greenowl403@gmail.com>
2020-05-26 19:59:41 +0300

    Add completed tasks

--- src/main/java/ru/ifmo/collections/KthLargest.java
+++ src/main/java/ru/ifmo/collections/KthLargest.java
@@ -1,18 +1,39 @@
 package ru.ifmo.collections;
 
+import java.util.PriorityQueue;
+
 /**
  * Design a class to find the kth largest element in a stream. k is from 1 to numbers.length.
  * Note that it is the kth largest element in the sorted order, not the kth distinct element.
- *
+ * <p>
  * Constructor accepts an integer k and an integer array numbers, which contains initial elements from the stream.
  * For each call to the method KthLargest.add(), return the element representing the kth largest element in the stream.
  */
+@SuppressWarnings("ConstantConditions")
 public class KthLargest {
+    private final PriorityQueue<Integer> queue = new PriorityQueue<>();
+    private int currentElement = 0;
+
     public KthLargest(int k, int[] numbers) {
-        // TODO implement
+        for (int number : numbers) {
+            queue.add(number);
+        }
+        cutQueue(k);
     }
 
     public int add(int val) {
-        throw new UnsupportedOperationException(); // TODO implement
+        if (val > currentElement) {
+            queue.add(val);
+            queue.poll();
+        }
+        currentElement = queue.peek();
+        return currentElement;
+    }
+
+    private void cutQueue(int k) {
+        while (queue.size() > k) {
+            queue.poll();
+        }
+        currentElement = queue.peek();
     }
 }
--- src/main/java/ru/ifmo/collections/SortedSet.java
+++ src/main/java/ru/ifmo/collections/SortedSet.java
@@ -1,36 +1,73 @@
 package ru.ifmo.collections;
 
 import java.util.AbstractSet;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 /**
  * Represents sorted set of unique values.
  * create() returns a SortedSet instance with natural ordering. (i.e. from smallest to largest in case of integer numbers)
  * from() is used to create a SortedSet instance with given Comparator.
  * Instances of a class can be created using only these two methods above.
- *
+ * <p>
  * This class should not be abstract and should be capable of adding/removing single/multiple elements.
  * It has two more methods: getSorted() and getReversed() which return an array of set contents in forward and reverse order respectively.
- *
+ * <p>
  * NB! This class must have only map(s) as an internal data structure(s).
  *
  * @param <T> set contents type
  */
-public abstract class SortedSet<T> extends AbstractSet<T> {
-    // private final Map<???, ???> contents; TODO decide Map implementation and key/value types. "???" is used just as an example
+public class SortedSet<T> extends AbstractSet<T> {
+    private final Map<T, T> contents;
+
     public static <T> SortedSet<T> create() {
-        throw new UnsupportedOperationException(); // TODO implement
+        return new SortedSet<>();
     }
 
     public static <T> SortedSet<T> from(Comparator<T> comparator) {
-        throw new UnsupportedOperationException(); // TODO implement
+        return new SortedSet<>(comparator);
     }
 
-    public T[] getSorted() {
-        throw new UnsupportedOperationException(); // TODO implement
+    public SortedSet(Comparator<T> comparator) {
+        contents = new TreeMap<>(comparator);
     }
 
-    public T[] getReversed() {
-        throw new UnsupportedOperationException(); // TODO implement
+    public SortedSet() {
+        this(null);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return contents.keySet().iterator();
+    }
+
+    @Override
+    public int size() {
+        return contents.size();
+    }
+
+    @Override
+    public boolean add(T element) {
+        if (contents.containsKey(element))
+            return false;
+        contents.put(element, element);
+        return true;
+    }
+
+    public List<T> getSorted() {
+        return new ArrayList<>(contents.keySet());
+    }
+
+    public List<T> getReversed() {
+        List<T> result = getSorted();
+        Collections.reverse(result);
+        return result;
     }
 }
